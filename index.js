953a7eb2ca8e14f42e1e80b7b5bc3a86a41c74e6
Peanutthechoosen <z-racer@hotmail.de>
2025-05-25 16:03:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'API läuft einwandfrei.' });
+});
+
+app.post('/npc/decide', (req, res) => {
+  const { situation } = req.body;
+  res.json({
+    decision: `Reaktion wird realistisch auf "${situation}" zurückgegeben.`
+  });
+});
+
+module.exports = app;
